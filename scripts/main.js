34689640988ce256dfd7bc2e442f3183a2cc2f5d
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-02-09 19:27:36 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,3 @@ Planets.gier.visible = true;
 Planets.gier.accessible = true;
 
 });
-
-Events.on(WorldLoadEvent, e => {
-
-    var parent = TechTree.all.find(u => u.content.name == "gr-gier");
-    
-    parent.each(node => {
-        if(node.content.unlocked){
-            node.content.clearUnlock();
-        }
-    });
-
-});
