2e96158b6445aaf5318abdcc9f6d13588f5a8c8e
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-02-10 21:38:29 +0700

    Update ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,38 +1,36 @@
-function setupUI() {
-    let overlaymarkerTable = Vars.ui.hudGroup.find("statustable");
-    overlaymarkerTable.row();
+function setupUI(){
+    let overlay = Vars.ui.hudGroup.find("statustable");
+    if(!overlay) return;
+
+    overlay.row();
 
     let tab = new Table();
-    overlaymarkerTable.add(tab).bottom().left();
+    overlay.add(tab).bottom().left();
 
-    tab.table(Tex.pane, t => {
-        let b = new Button(Styles.none);
-        let icon = new TextureRegionDrawable(Blocks.surgeWall.uiIcon);
-        b.button(icon, () => {
+    tab.table(Tex.pane, table => {
 
-        });
-        t.add(b);
+        table.button(
+            new TextureRegionDrawable(Blocks.surgeWall.uiIcon),
+            Styles.none,
+            () => {
 
-        t.clicked(() => {
-            Vars.ui.showTextInput("", "Cheat", 32768, "", false, text => {
+                Vars.ui.showTextInput("", "Cheat", 32768, "", false, text => {
 
-                if (text == "dagger"){
-                let playerUnit = Vars.player.unit();
-                let x = playerUnit.x;
-                let y = playerUnit.y;
-                let ro = playerUnit.rotation;
-                let t = playerUnit.team;
-                UnitTypes.dagger.spawn(t,x,y,ro);
-                }
-                
-            })
-        })
-    })
+                    if(text == "dagger"){
+                        let u = Vars.player.unit();
+                        if(!u) return;
 
-    tab.visibility = () => {
-        // return (Vars.ui.hudfrag.shown && Vars.mobile && !Vars.net.client() ? true : false)
-        return (Vars.ui.hudfrag.shown && !Vars.net.client() ? true : false)
-    }
+                        UnitTypes.dagger.spawn(u.team, u.x, u.y, u.rotation);
+                    }
+
+                });
+
+            }
+        );
+
+    });
+
+    tab.visibility = () => Vars.ui.hudfrag.shown && !Vars.net.client();
 }
 
 module.exports = {
