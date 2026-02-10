dd02dc9f4e61926ae1fba69a533fa98cb4de3103
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-02-10 21:04:35 +0700

    Create ui.js

---
+++ scripts/ui.js
@@ -0,0 +1,31 @@
+function setupUI() {
+    let overlaymarkerTable = Vars.ui.hudGroup.find("statustable");
+    overlaymarkerTable.row();
+
+    let tab = new Table();
+    overlaymarkerTable.add(tab).bottom().left();
+
+    tab.table(Tex.pane, t => {
+        let b = new Button(Styles.none);
+        let icon = new TextureRegionDrawable(Blocks.surgeWall.uiIcon);
+        b.button(icon, () => {
+
+        });
+        t.add(b);
+
+        t.clicked(() => {
+            Vars.ui.showTextInput("", "Cheat", 32768, "", false, text => {
+
+            })
+        })
+    })
+
+    tab.visibility = () => {
+        // return (Vars.ui.hudfrag.shown && Vars.mobile && !Vars.net.client() ? true : false)
+        return (Vars.ui.hudfrag.shown && !Vars.net.client() ? true : false)
+    }
+}
+
+module.exports = {
+    setupUI: setupUI,
+}
