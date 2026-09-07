2eeac457974d90812a70fb5bf9a3935ae8e0b58a
Fat Bird’s Owner <182750122+Fat-Bird-Owner@users.noreply.github.com>
2026-09-07 18:49:21 +0700

    Update IconLoader.js

--- scripts/IconLoader.js
+++ scripts/IconLoader.js
@@ -1,33 +1,41 @@
-const textures = [
-    "gr-techtree-kela",
-    "gr-gier",
-    "gr-copper-fort",
-    "gr-mass-launcher-base"
-];
 
 Events.on(ClientLoadEvent, () => {
-    let code = 0xE000;
 
-    for(let i = 0; i < textures.length; i++){
-        let name = textures[i];
-        let region = Core.atlas.find(name);
+    // code ripped straight from psammos
+    loadIcon(61106, "gr-cualbult-foundry") // \uEEB2
 
-        if(!region.found()){
-            Log.err("Icon texture not found: " + name);
-            continue;
-        }
+    function loadIcon(id, regionName) {
+        let fonts = Seq.with(Fonts.def, Fonts.outline);
+        let uitex = Core.atlas.find("logo").texture;
+        let size = Math.floor(Fonts.def.getData().lineHeight / Fonts.def.getData().scaleY);
 
-        Fonts.registerIcon(
-            name,
-            region.name,
-            code + i,
-            region
-        );
+        let region = Core.atlas.find(regionName);
 
-        Log.info(
-            "Registered " + name +
-            " -> " + region.name +
-            " [" + (code + i) + "]"
-        );
-    }
-});
+        if(region.texture != uitex) {
+            return;
+        };
+
+        let out = Scaling.fit.apply(region.width, region.height, size, size);
+
+        let glyph = new Font.Glyph();
+        glyph.id = id;
+        glyph.srcX = 0;
+        glyph.srcY = 0;
+        glyph.width = Math.floor(out.x);
+        glyph.height = Math.floor(out.y);
+        glyph.u = region.u;
+        glyph.v = region.v2;
+        glyph.u2 = region.u2;
+        glyph.v2 = region.v;
+        glyph.xoffset = 0;
+        glyph.yoffset = -size;
+        glyph.xadvance = size;
+        glyph.kerning = null;
+        glyph.fixedWidth = true;
+        glyph.page = 0;
+        fonts.each((f) => {
+            f.getData().setGlyph(id, glyph);
+        });
+    };
+
+})
