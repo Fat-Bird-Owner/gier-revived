c8451774fc1179767ac86b164a29cf641628157b
Fat Bird’s Owner <182750122+Fat-Bird-Owner@users.noreply.github.com>
2026-09-07 12:56:15 +0700

    Update effects.js

--- scripts/effects.js
+++ scripts/effects.js
@@ -237,7 +237,7 @@ Draw.reset()
 const targetPointEffect = new Effect(1, e => {
     if (Vars.state.isPaused()) return;
     let u = Vars.content.unit("gr-target-point");
-    if (u) u.spawn(Team.get(Team.all.size), e.x, e.y, e.rotation);
+    if (u) u.spawn(Team.get(Team.get(Team.all.length - 1)), e.x, e.y, e.rotation);
 });
 
 exports.fissureAvailable = fissureAvailable;
