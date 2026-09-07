7753a338ff92d91c071c0ed2cd7c8f70bfe786c0
Fat Bird’s Owner <182750122+Fat-Bird-Owner@users.noreply.github.com>
2026-09-07 21:07:42 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,7 @@ Log.err("[red]" + txt + "[] - Failed to compile")
   
 // Team changes, showing gier and other stuff
 //require("EventRunnable"); Unreliable
+compile("IconLoader");
 compile("Transcripts");
 compile("Startup");
 compile("Deranged");
@@ -80,45 +81,3 @@ compile("StealthTemplate");
 } catch(e){
 log(e)
 }
-
-Events.on(ClientLoadEvent, () => {
-
-    // code ripped straight from psammos
-    loadIcon(61106, "gr-exposed") // \uEEB2
-
-    function loadIcon(id, regionName) {
-        let fonts = Seq.with(Fonts.def, Fonts.outline);
-        let uitex = Core.atlas.find("logo").texture;
-        let size = Math.floor(Fonts.def.getData().lineHeight / Fonts.def.getData().scaleY);
-
-        let region = Core.atlas.find(regionName);
-
-        if(region.texture != uitex) {
-            return;
-        };
-
-        let out = Scaling.fit.apply(region.width, region.height, size, size);
-
-        let glyph = new Font.Glyph();
-        glyph.id = id;
-        glyph.srcX = 0;
-        glyph.srcY = 0;
-        glyph.width = Math.floor(out.x);
-        glyph.height = Math.floor(out.y);
-        glyph.u = region.u;
-        glyph.v = region.v2;
-        glyph.u2 = region.u2;
-        glyph.v2 = region.v;
-        glyph.xoffset = 0;
-        glyph.yoffset = -size;
-        glyph.xadvance = size;
-        glyph.kerning = null;
-        glyph.fixedWidth = true;
-        glyph.page = 0;
-        fonts.each((f) => {
-            f.getData().setGlyph(id, glyph);
-        });
-    };
-
-  Team.green.emoji = "\uEEB2";
-})
